d40e9b523557c5cc910010c3d43381c48e9f98c8
AliKhodadad <alikhodadad44@gmail.com>
2016-02-07 14:38:01 +0330

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return " Telewild نــرخـ ساختـ گروهـ🔽\nیک  گروهـ یک ماهـ 2000 تومانـ"
+end
+return {
+  description = "Nerkh Sakht Group", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/nerkh$",
+    "^!nerkh$",
+    "^nerkh$",
+    "^nerkh$",
+   "^/Nerkh$",
+   "^!Nerkh$",
+   "^Nerkh$",
+   "^نرخ$",
+
+  },
+  run = run
+}
+end
