d765cfd972508da805c64b550092f4b9e0bf1822
AliKhodadad <alikhodadad44@gmail.com>
2016-02-07 12:38:31 +0330

    Update text.lua

--- plugins/text.lua
+++ plugins/text.lua
@@ -20,10 +20,10 @@ end
 return {
   description = "Convert LaTeX equation to image",
   usage = {
-    "!text [equation]: Convert LaTeX equation to image"
+    "text [equation]: Convert LaTeX equation to image"
   },
   patterns = {
-    "^!text (.+)$"
+    "^[Tt]text (.+)$"
   },
   run = run
 }
